a91fffc669341c1dc94bf558c3e30c03941fc623
qudusolamide13@gmail.com <qudus@LAPTOP-IK2MU3QS.localdomain>
2022-07-04 14:32:29 +0100

    first commit

---
+++ udagram-api-feed/src/server.ts
@@ -0,0 +1,44 @@
+import cors from 'cors';
+import express from 'express';
+import {sequelize} from './sequelize';
+
+import {IndexRouter} from './controllers/v0/index.router';
+
+import bodyParser from 'body-parser';
+import {config} from './config/config';
+import {V0MODELS} from './controllers/v0/model.index';
+
+const c = config.dev;
+
+(async () => {
+  await sequelize.addModels(V0MODELS);
+  await sequelize.sync();
+
+  const app = express();
+  const port = process.env.PORT || 8080;
+
+  app.use(bodyParser.json());
+
+  // We set the CORS origin to * so that we don't need to
+  // worry about the complexities of CORS this lesson. It's
+  // something that will be covered in the next course.
+  app.use(function(req, res, next) {
+	  res.header("Access-Control-Allow-Origin", c.url);
+	  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	  next();
+  });
+
+  app.use('/api/v0/', IndexRouter);
+
+  // Root URI call
+  app.get( '/', async ( req, res ) => {
+    res.send( '/api/v0/' );
+  } );
+
+
+  // Start the Server
+  app.listen( port, () => {
+    console.log( `server running ` + c.url );
+    console.log( `press CTRL+C to stop server` );
+  } );
+})();
